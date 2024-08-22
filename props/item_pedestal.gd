4ce8b93e0dd27051d9bf876f34c157584b9d0062
Yokcos <yokcube@gmail.com>
2024-08-22 11:13:55 +0100

    pedestals can now force a certain item to spawn

--- props/item_pedestal.gd
+++ props/item_pedestal.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+## List of items that can spawn at this pedestal
 @export var items: Array[PackedScene] = []
+## Speed with which items jump when they appear from this pedestal
 @export var item_jump_speed := 200.0
+## If set, this item will always spawn from this pedestal. If not, will randomly select from [code]items[/code]
+@export var force_item: PackedScene = null
 
 var current_item: Item = null
 
@@ -10,7 +14,11 @@ func _ready():
 		$spawn_timer.start()
 
 func spawn_item():
-	var this_item = items.pick_random()
+	var this_item: PackedScene
+	if force_item:
+		this_item = force_item
+	else:
+		this_item = items.pick_random()
 	var new_item: Item = MattohaSystem.CreateInstance(this_item.resource_path)
 	new_item.global_position = position
 	MattohaSystem.GetLobbyNodeFor(self).add_child(new_item)
