a99079ee3f35dae12c7c73415274744e5ca48c77
Yokcos <yokcube@gmail.com>
2024-09-07 10:15:58 +0100

    netcode respawn now synced

--- multiplayer/maps/multiplayer_arena.gd
+++ multiplayer/maps/multiplayer_arena.gd
@@ -6,6 +6,7 @@ extends Node2D
 func _ready():
 	Game.world = self
 	
+	# If this is a client, request the addition of a new player object
 	if !multiplayer.is_server():
 		rpc_add_new_player.bind( multiplayer.get_unique_id() ).rpc_id(1)
 		var ui = map_ui.instantiate()
@@ -17,12 +18,14 @@ func _process(_delta):
 
 @rpc("any_peer", "call_remote", "reliable")
 func rpc_add_new_player(peer: int):
-	add_player.rpc(peer)
+	#add_player.rpc(peer)
+	var new_player: PlayerStats = PlayerManager.add_player(0, peer)
+	Game.print_multiplayer("Arena: Spawned player with owner %s, authority %s. is owner: %s" % [peer, new_player.multiplayer_owner, new_player.object.is_owner()])
 
 @rpc("authority", "call_local", "reliable")
 func add_player(peer: int):
-	var new_player: Player = PlayerManager.spawn_player(0, peer)
-	Game.print_multiplayer("Arena: Spawned player with owner %s, authority %s. is owner: %s" % [peer, new_player.get_multiplayer_authority(), new_player.is_owner()])
+	var new_player: PlayerStats = PlayerManager.add_player(0, peer)
+	Game.print_multiplayer("Arena: Spawned player with owner %s, authority %s. is owner: %s" % [peer, new_player.multiplayer_owner, new_player.object.is_owner()])
 
 func check_player_lives():
 	if multiplayer.is_server():
