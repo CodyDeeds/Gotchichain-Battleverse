717ab6a7111f19b34d45df03ffa283ada877cdc7
Yokcos <yokcube@gmail.com>
2024-09-14 08:53:16 +0100

    projectiles and items now handle sound effects through GlobalSound

--- projectiles/fireball.gd
+++ projectiles/fireball.gd
@@ -4,24 +4,11 @@ extends Projectile
 @export var projectile_gravity: float = 400
 ## How fast the fireball jumps after hitting the ground
 @export var bounce_speed: float = 200
-## Sound effect for the fireball
-@export var fireball_sound: AudioStream = preload("res://items/fireball.ogg")
 
 func _ready() -> void:
 	super()
 	
 	acceleration = Vector2(0, projectile_gravity)
-
-	# Initialize and configure the sound player if it's not already set
-	if sound_player == null:
-		sound_player = AudioStreamPlayer.new()
-		add_child(sound_player)
-	sound_player.stream = fireball_sound
-	sound_player.volume_db = -15  # Set the volume to a quieter level
-	print("Fireball: Sound player initialized with volume: ", sound_player.volume_db)
-	
-	# Play the sound when the fireball is created
-	sound_player.play()
 
 func _process(delta: float) -> void:
 	super(delta)
@@ -31,9 +18,8 @@ func _process(delta: float) -> void:
 	if $downcast.is_colliding():
 		velocity.y = min(velocity.y, -bounce_speed)
 		# Play the sound when the fireball bounces
-		sound_player.play()
+		#sound_player.play()
 		
 	if $rightcast.is_colliding() or $leftcast.is_colliding():
 		# Play the sound when the fireball dies
-		sound_player.play()
 		die()
