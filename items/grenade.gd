513c0f3ac075092b2cc0777c31687e89ef5989f9
Yokcos <yokcube@gmail.com>
2024-09-14 09:09:08 +0100

    added SFX for players taking damage and improved grenade SFX

--- items/grenade.gd
+++ items/grenade.gd
@@ -1,44 +1,30 @@
 @tool
 extends Item
 
-@export var explosion_sound: AudioStream = preload("res://audio/sfx/bomb.ogg")
-
-var sound_player: AudioStreamPlayer = null
-var sound_timer: Timer = null
+@export var explosion_sound: StringName = &""
+@export var beep_sound: StringName = &""
 
 func _ready() -> void:
 	super()
-	initialize_sound_player()
 
-func initialize_sound_player():
-	if sound_player == null:
-		sound_player = AudioStreamPlayer.new()
-		get_tree().root.add_child(sound_player)  # Add to the scene tree root, not as a child of grenade
-		sound_player.stream = explosion_sound
-		sound_player.volume_db = -15  # Set the volume to a quieter level
-		#print("Grenade: Sound player initialized with volume: ", sound_player.volume_db)
-	
-	if sound_timer == null:
-		sound_timer = Timer.new()
-		get_tree().root.add_child(sound_timer)  # Add to the scene tree root, not as a child of grenade
-		sound_timer.wait_time = 5.78
-		sound_timer.one_shot = true
-		if not sound_timer.is_connected("timeout", Callable(self, "_on_sound_finished")):
-			sound_timer.connect("timeout", Callable(self, "_on_sound_finished"))
 
 func get_activated():
 	super()
-	#print("Grenade: Activated")
-	initialize_sound_player()
 	var peers = MattohaSystem.Client.GetLobbyPlayersIds()
 	for peer_id in peers:
-		rpc_id(peer_id, "rpc_flash")
-	play_sound()
+		rpc_start_timer.rpc_id(peer_id)
 
 @rpc("any_peer", "call_local", "reliable")
-func rpc_flash():
-	play_sound()
+func rpc_start_timer():
 	$animator.play("flash")
+
+func flash():
+	%sprite.frame = 1
+	var timer := get_tree().create_timer(.1)
+	timer.timeout.connect(func():
+		%sprite.frame = 0
+	)
+	GlobalSound.play_sfx_2d("beep", global_position)
 
 func explode():
 	var peers = MattohaSystem.Client.GetLobbyPlayersIds()
@@ -48,17 +34,7 @@ func explode():
 @rpc("any_peer", "call_local", "reliable")
 func rpc_explode():
 	%explosion_spawner.activate()
+	GlobalSound.play_sfx_2d(explosion_sound, global_position)
 	#print("Grenade: Explosion triggered, waiting for sound to finish")
 	queue_free()  # Ensure grenade is removed immediately after the explosion
 
-func play_sound():
-	if sound_player and sound_player.stream:
-		sound_player.play()
-		sound_timer.start()
-		#print("Grenade: Playing sound: ", sound_player.stream)
-	#else:
-		#print("Grenade: Sound player or stream not initialized")
-
-func _on_sound_finished():
-	sound_player.queue_free()
-	#print("Grenade: Queue free after sound finished playing")
