c9433591b61fbed849f0b4fec9d5a026bd0d458b
Yokcos <yokcube@gmail.com>
2024-10-14 14:37:24 +0100

    chests now close 10 seconds after opening, and give their contents directly to the player who bought

--- props/chest.gd
+++ props/chest.gd
@@ -13,16 +13,30 @@ func _ready() -> void:
 	show_cost()
 
 
-func deploy_item():
-	var this_item: PackedScene = BigData.get_random_item_from_pool(Item.POOLS.CHEST)
-	var new_item: Item = Game.create_instance(this_item)
-	Game.deploy_instance(new_item, global_position)
-	var impulse: Vector2 = Vector2(0, -item_jump_speed)
-	impulse = impulse.rotated( deg_to_rad(randf_range(-item_random_angle, item_random_angle)) )
-	new_item.apply_central_impulse(impulse)
+func deploy_item(player: int = 0):
+	if PlayerManager.players.size() > player:
+		# Place this item into the player's hand
+		var player_object: Player = PlayerManager.players[player].object
+		player_object.spawn_item(BigData.get_random_item_from_pool(Item.POOLS.CHEST))
+	else:
+		# If there's no player, chuck the item at a suitably amusing speed
+		var this_item: PackedScene = BigData.get_random_item_from_pool(Item.POOLS.CHEST)
+		var new_item: Item = Game.create_instance(this_item)
+		Game.deploy_instance(new_item, global_position)
+		var impulse: Vector2 = Vector2(0, -item_jump_speed)
+		impulse = impulse.rotated( deg_to_rad(randf_range(-item_random_angle, item_random_angle)) )
+		new_item.apply_central_impulse(impulse)
 
 func show_cost():
 	%cost.text = "[center][wave]$%s[/wave][/center]" % cost
+
+func open():
+	%sprite.frame = 1
+	%interactable.active = false
+
+func close():
+	%sprite.frame = 0
+	%interactable.active = true
 
 
 func _on_interactable_activated(player: int) -> void:
@@ -30,6 +44,10 @@ func _on_interactable_activated(player: int) -> void:
 		var available_money: int = PlayerManager.players[player].money
 		if available_money >= cost:
 			PlayerManager.players[player].money -= cost
-			%sprite.frame = 1
-			%interactable.active = false
-			deploy_item()
+			open()
+			deploy_item(player)
+			%timer.start()
+
+
+func _on_timer_timeout() -> void:
+	close()
