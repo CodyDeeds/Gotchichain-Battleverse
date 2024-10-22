8a979ad4b1726bf2dd0d58a0fd041e9055094d11
Yokcos <yokcube@gmail.com>
2024-10-22 11:20:25 +0100

    interactable tooltip is a plain down arrow for the sake of chests, which also now announce when a person trying to buy them is too poor

--- props/chest.gd
+++ props/chest.gd
@@ -5,12 +5,22 @@ extends Node2D
 @export var item_jump_speed: float = 500.0
 ## Item will be thrown upward plus or minus this random angle value in degrees
 @export var item_random_angle: float = 30.0
-
+## Cost to open the chest
 @export var cost: int = 1000
+## Amount of time the chest announces that the attempted opener is too poor if they try to open the chest without the necessary monies
+@export var too_poor_duration: float = 2
+
+var too_poor_time: float = 0
 
 
 func _ready() -> void:
 	show_cost()
+
+func _process(delta: float) -> void:
+	if too_poor_time > 0:
+		too_poor_time -= delta
+		if too_poor_time <= 0:
+			show_cost()
 
 
 func deploy_item(player: int = 0):
@@ -28,6 +38,7 @@ func deploy_item(player: int = 0):
 		new_item.apply_central_impulse(impulse)
 
 func show_cost():
+	%cost.modulate = Color(1, 1, 1, 1)
 	%cost.text = "[center][wave]$%s[/wave][/center]" % cost
 
 func open():
@@ -47,6 +58,10 @@ func _on_interactable_activated(player: int) -> void:
 			open()
 			deploy_item(player)
 			%timer.start()
+		else:
+			too_poor_time = too_poor_duration
+			%cost.text = "[center]TOO POOR![/center]"
+			%cost.modulate = Color(1, 0, 0, 1)
 
 
 func _on_timer_timeout() -> void:
