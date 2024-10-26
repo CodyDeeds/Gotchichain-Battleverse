5bdab7cea824f0c3bed4c1dbb386f2bf50bc5efd
Yokcos <yokcube@gmail.com>
2024-10-26 08:05:07 +0100

    added escalating sfx to coin collection

--- props/coin.gd
+++ props/coin.gd
@@ -1,7 +1,13 @@
 extends RigidBody2D
 
 
+## Amount to increase the pitch of the coin collection SFX when a coin is grabbed
+@export var pitch_increase_per_coin: float = 0.01
+## Speed to revert the SFX pitch to normal
+@export var pitch_decay_rate: float = 5
+
 var value: int = 100
+var sfx_pitch: float = 1
 
 const fx_scene = preload("res://fx/coin_collect.tscn")
 
@@ -12,6 +18,9 @@ func _init() -> void:
 func _ready() -> void:
 	apply_central_impulse( Vector2(0, randf_range(-600, -100)).rotated(randf_range(-.5, .5)) )
 	%sprite.actual_frame = randf()*8
+
+func _process(delta: float) -> void:
+	sfx_pitch = lerp(sfx_pitch, 1.0, pitch_decay_rate*delta)
 
 
 func change_scale(what: float):
@@ -38,5 +47,9 @@ func _on_player_detector_player_entered() -> void:
 	Game.deploy_instance(new_fx, global_position)
 	new_fx.scale = scale
 	
+	var new_sfx = GlobalSound.play_sfx_2d(&"coins", global_position)
+	new_sfx.pitch_scale = sfx_pitch
+	for i in get_tree().get_nodes_in_group(&"coins"):
+		i.sfx_pitch += pitch_increase_per_coin
 	awaken_nearby_coins()
 	queue_free()
