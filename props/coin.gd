7dbcf916e38b587d0a9314e3cdb5d354dd8d36ad
Yokcos <yokcube@gmail.com>
2024-10-26 07:03:57 +0100

    coins now awaken when a nearby coin is collected, allowing them to not float in the air

--- props/coin.gd
+++ props/coin.gd
@@ -6,6 +6,9 @@ var value: int = 100
 const fx_scene = preload("res://fx/coin_collect.tscn")
 
 
+func _init() -> void:
+	add_to_group(&"coins")
+
 func _ready() -> void:
 	apply_central_impulse( Vector2(0, randf_range(-600, -100)).rotated(randf_range(-.5, .5)) )
 	%sprite.actual_frame = randf()*8
@@ -16,6 +19,12 @@ func change_scale(what: float):
 	%shape.scale = this_scale
 	%sprite.scale = this_scale
 	%player_detector.scale = this_scale
+
+func awaken_nearby_coins():
+	var awaken_range: float = 64
+	for this_coin in get_tree().get_nodes_in_group(&"coins"):
+		if global_position.distance_squared_to(this_coin.global_position) < awaken_range*awaken_range:
+			this_coin.sleeping = false
 
 
 func _on_player_detector_player_entered() -> void:
@@ -29,4 +38,5 @@ func _on_player_detector_player_entered() -> void:
 	Game.deploy_instance(new_fx, global_position)
 	new_fx.scale = scale
 	
+	awaken_nearby_coins()
 	queue_free()
