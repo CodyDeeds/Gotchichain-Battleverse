f579a6e93f982f7dc00852cf0940eadbcbb23f40
Yokcos <yokcube@gmail.com>
2024-10-26 07:59:02 +0100

    added particle effects to crate destruction

--- props/crate_money.gd
+++ props/crate_money.gd
@@ -5,6 +5,8 @@ extends Node2D
 @export var value_weight: float = 0.05
 var value: int = 0
 
+const particle_scene = preload("res://fx/crate_break.tscn")
+
 
 func _init() -> void:
 	add_to_group(&"value_sources")
@@ -20,4 +22,6 @@ func deploy_coins():
 
 func _on_hurtbox_hit(_hitbox) -> void:
 	deploy_coins()
+	var new_particles = particle_scene.instantiate()
+	Game.deploy_instance(new_particles, global_position)
 	queue_free()
